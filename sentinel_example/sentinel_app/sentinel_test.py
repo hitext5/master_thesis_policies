9775a53e4b3e63691c4ef699dfcb77df1245b530
Jan Magnusson <jan.magnusson@unibw.de>
2023-03-22 14:32:14 -0400

    method name corrected

--- sentinel_example/sentinel_app/sentinel_test.py
+++ sentinel_example/sentinel_app/sentinel_test.py
@@ -4,7 +4,7 @@ from electronic_device import ElectronicDevice
 from smart_plug import SmartPlug
 
 
-def test_basic_casbin():
+def test_basic_sentinel():
     # This function updates the sentinel policy file with the new plug rated power
     def update_sentinel_policy(new_content: str):
         with open("sentinel_policy.sentinel", "r") as file:
@@ -24,7 +24,7 @@ def test_basic_casbin():
                 return True
             else:
                 return False
-        except subprocess.CalledProcessError as e:
+        except subprocess.CalledProcessError:
             return False
     # Create an instance of ElectronicDevice with a work_power of 40
     hairdryer = ElectronicDevice(work_power=40)
@@ -39,15 +39,15 @@ def test_basic_casbin():
     plug = SmartPlug(rated_power=90, plugged_devices=[], slots=5)
 
     update_sentinel_policy(f'plug_slots = {plug.slots}\nplug_rated_power = {plug.rated_power}\n'
-                           f'electronic_device_work_power = {hairdryer.work_power}\ninput_act = "plug_in" \n\n')
+                           f'electronic_device_work_power = {hairdryer.work_power}\ninput_act = "plug_in"\n\n')
     assert apply_policy()
     plug.plug_in(hairdryer)
 
     update_sentinel_policy(f'plug_slots = {plug.slots}\nplug_rated_power = {plug.rated_power}\n'
-                           f'electronic_device_work_power = {fan.work_power}\ninput_act = "plug_in" \n\n')
+                           f'electronic_device_work_power = {fan.work_power}\ninput_act = "plug_in"\n\n')
     assert apply_policy()
     plug.plug_in(fan)
 
     update_sentinel_policy(f'plug_slots = {plug.slots}\nplug_rated_power = {plug.rated_power}\n'
-                           f'electronic_device_work_power = {charger.work_power}\ninput_act = "plug_in" \n\n')
+                           f'electronic_device_work_power = {charger.work_power}\ninput_act = "plug_in"\n\n')
     assert not apply_policy()
