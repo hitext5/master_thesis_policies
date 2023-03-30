a55116a60bed16d16ec6459a8cf665afa95640bd
janma <jan.magnusson@unibw.de>
2023-03-30 14:00:01 -0400

    casbin example with list and function r.electronic_device in the casbin_policy.csv has to be adjusted to [0] in order to apply the list access. test(r.electronic_device, r.smart_plug) in casbin_model.conf if the function should be used.

--- casbin_example/casbin_app/casbin_test.py
+++ casbin_example/casbin_app/casbin_test.py
@@ -4,16 +4,29 @@ from electronic_device import ElectronicDevice
 from smart_plug import SmartPlug
 
 
+# def overall_power(objects: list[ElectronicDevice], subject: SmartPlug):
+#     overall = 0
+#     for obj in objects:
+#         overall += obj.work_power
+#     if subject.rated_power >= overall:
+#         return True
+#     else:
+#         return False
+#
+
+
 # Check if the device is allowed to plug into the smart plug
 def test_basic_casbin():
     # Initialize the Casbin object.
     e = casbin.Enforcer("casbin_model.conf", "casbin_policy.csv")
 
+    # e.add_function("test", overall_power)
+
     # Create an instance of ElectronicDevice with a work_power of 40
     hairdryer = ElectronicDevice(work_power=40)
 
     # Create an instance of ElectronicDevice with a work_power of 30
-    fan = ElectronicDevice(work_power=30)
+    fan = ElectronicDevice(work_power=100)
 
     # Create an instance of ElectronicDevice with a work_power of 30
     charger = ElectronicDevice(work_power=30)
@@ -21,8 +34,10 @@ def test_basic_casbin():
     # Create an instance of SmartPlug with a rated_power of 90 and an empty list for plugged_devices
     plug = SmartPlug(rated_power=90, plugged_devices=[], slots=5)
 
-    assert e.enforce(hairdryer, plug, "plug_in")
-    plug.plug_in(hairdryer)
+    # obj_list = [hairdryer, fan]
+
+    # assert e.enforce(obj_list, plug, "plug_in")
+    # plug.plug_in(hairdryer)
 
     assert e.enforce(fan, plug, "plug_in")
     plug.plug_in(fan)
