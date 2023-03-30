69672a60726b66e3de96d3bc22f4c51d328b0ada
janma <jan.magnusson@unibw.de>
2023-03-30 14:21:12 -0400

    casbin example with multiple policies

--- casbin_example/casbin_app/casbin_test.py
+++ casbin_example/casbin_app/casbin_test.py
@@ -23,23 +23,23 @@ def test_basic_casbin():
     # e.add_function("test", overall_power)
 
     # Create an instance of ElectronicDevice with a work_power of 40
-    hairdryer = ElectronicDevice(work_power=40)
+    hairdryer = ElectronicDevice(work_power=100)
 
     # Create an instance of ElectronicDevice with a work_power of 30
-    fan = ElectronicDevice(work_power=100)
+    fan = ElectronicDevice(work_power=50)
 
     # Create an instance of ElectronicDevice with a work_power of 30
-    charger = ElectronicDevice(work_power=30)
+    charger = ElectronicDevice(work_power=60)
 
     # Create an instance of SmartPlug with a rated_power of 90 and an empty list for plugged_devices
     plug = SmartPlug(rated_power=90, plugged_devices=[], slots=5)
 
-    # obj_list = [hairdryer, fan]
+    obj_list = [hairdryer, fan]
 
-    # assert e.enforce(obj_list, plug, "plug_in")
-    # plug.plug_in(hairdryer)
+    assert e.enforce(obj_list, plug, "plug_in")
+    plug.plug_in(hairdryer)
 
-    assert e.enforce(fan, plug, "plug_in")
-    plug.plug_in(fan)
-
-    assert not e.enforce(charger, plug, "plug_in")
+    # assert e.enforce(fan, plug, "plug_in")
+    # plug.plug_in(fan)
+    #
+    # assert not e.enforce(charger, plug, "plug_in")
